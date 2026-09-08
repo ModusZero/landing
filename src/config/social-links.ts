af7fda58a255fc685ba2f688d56b1e5757235dda
Luis Alberto Hernández Roselló <luisalbertohedzro@gmail.com>
2026-09-08 15:24:49 -0400

    Update social links with new GitHub and LinkedIn URLs

--- src/config/social-links.ts
+++ src/config/social-links.ts
@@ -1,11 +1,11 @@
 import type { IconName } from "@/types/icon-name";
 
 export const SOCIAL_LINKS = [
-    { name: 'GitHub', href: 'https://github.com/Luke1606', icon: 'github' },
-    { name: 'LinkedIn', href: 'https://linkedin.com/in/luke1060-dev', icon: 'linkedin' },
-    { name: 'Facebook', href: 'https://www.facebook.com/luis.hernandez.rosello/', icon: 'facebook' },
+    { name: 'GitHub', href: 'https://github.com/luishrosello', icon: 'github' },
+    { name: 'LinkedIn', href: 'https://linkedin.com/in/luishrosello', icon: 'linkedin' },
+    { name: 'Facebook', href: 'https://www.facebook.com/luis.hernandez.rosello', icon: 'facebook' },
+    { name: 'Instagram', href: 'https://www.instagram.com/luishrosello', icon: 'instagram' },
     { name: 'Threads', href: 'https://x.com/LuisAlbert21854', icon: 'threads' },
-    { name: 'Instagram', href: 'https://www.instagram.com/luke1606/', icon: 'instagram' },
     { name: 'X', href: 'https://x.com/LuisAlbert21854', icon: 'twitter' },
 ] as {
     name: string;
